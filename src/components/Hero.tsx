ef45e2a710c0b5b8d389830dddae51011611a9d3
Hussain101 <hussainsidd88@gmail.com>
2025-03-15 15:15:16 +0500

    text changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,18 +3,9 @@ export const Hero = () => {
 		<section className="h-screen relative bg-purple-900 ">
 			<div className="absolute top-[40%] left-20 z-20">
 				<h1 className="text-5xl text-white font-mynerver font-bold">
-					Air Not-So-Jordans
-				</h1>
+				Air Style Jordans				</h1>
 				<p className="w-[500px] pt-2 text-white text-justify text-sm">
-					Introducing the Air Not-So-Jordans - the perfect sneakers
-					for those who want to flex on a budget! These shoes may not
-					be the real deal, but who needs authenticity when you can
-					have imitation? Plus, these babies are so comfortable, you
-					won't even notice the difference. They're the kind of shoes
-					that make you say, 'Are those Jordans?' and you can
-					confidently reply, 'Nah, but they're Air Not-So-Jordans!' So
-					go ahead, rock these kicks with pride and let everyone know
-					that you're frugal and fashionable all at once.
+				Introducing the Air Inspired Jordans – the perfect sneakers for those who love style without the hefty price tag! These kicks bring the same bold look and comfort as the originals, so you can step out with confidence. They're so sleek and comfy that people might just ask, "Are those Jordans?" And you can proudly say, "Not quite, but they’ve got the same energy!" So go ahead—rock these sneakers with pride, knowing you're making a smart, stylish choice that turns heads without breaking the bank!
 				</p>
 
 				<button
